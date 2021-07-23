4d2d1cac59310219bc0883a010a800a2e75b1475
Shivangi1997Tripathi <55496011+Shivangi1997Tripathi@users.noreply.github.com>
2021-07-23 14:19:22 +0530

    Update square.java

--- square.java
+++ square.java
@@ -1,13 +1,21 @@
 public class CalculateSquare{
   
-  public static vpid main(String args){
+  public static void main(String args[]){
     double squareofNumber = square(9);
+    double cubeofNumber = cube(9);
+    
     System.out.println(squareofNumber);
+    System.out.println(cubeofNumber);
+    
     
   }
   
   public Double square(Integer num){
     return (Math.pow(num,2));
+ }
+  
+    public Double cube(Integer num){
+    return (Math.pow(num,3));
  }
   
 }
